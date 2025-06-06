d7f36c60f8ff2b52af77c2dccfde44133c95844c
Paruyr Yeprikyan <60259252+paur94@users.noreply.github.com>
2025-06-06 20:07:00 +0400

    Create Route.ts

---
+++ Models/Route.ts
@@ -0,0 +1,82 @@
+import { Exchange, ExchangeToken } from "./Exchange";
+import { NetworkRouteToken, NetworkRoute } from "./Network";
+
+export type RouteToken = NetworkRouteToken | ExchangeToken
+export type Route = ({ cex: true } & Exchange | { cex?: false } & NetworkRoute)
+
+export type NetworkElement = {
+    type: 'network';
+    route: NetworkRoute & { cex: false };
+}
+export type ExchangeElement = {
+    type: 'exchange';
+    route: Exchange & { cex: true };
+}
+export type NetworkTokenElement = {
+    type: 'network_token';
+    route: {
+        token: RouteToken;
+        route: NetworkRoute & { cex: false };
+    }
+}
+export type ExchangeTokenElement = {
+    type: 'exchange_token';
+    route: {
+        token: RouteToken;
+        route: Exchange & { cex: true };
+    }
+}
+export type TitleElement = {
+    type: 'group_title';
+    text: string
+}
+export type GroupedTokenElement = {
+    type: 'grouped_token';
+    symbol: string;
+    items: (NetworkTokenElement | ExchangeTokenElement)[];
+}
+export type RowElement = {}
+    & (NetworkElement
+        | ExchangeElement
+        | NetworkTokenElement
+        | ExchangeTokenElement
+        | TitleElement
+        | GroupedTokenElement)
+
+
+
+export type _Route =
+    ({
+        route_type: 'network';
+    } & NetworkRoute)
+    | ({
+        route_type: 'exchange';
+    } & Exchange)
+    | ({
+        route_type: 'token';
+    } & {
+        token: RouteToken;
+        network: NetworkRoute;
+    })
+
+export type _RoutesGroup = {
+    name: string;
+} & (
+        {
+            type: 'network';
+            routes: NetworkRoute[];
+        }
+        |
+        {
+            type: 'exchange';
+            routes: Exchange[];
+        }
+        |
+        {
+            type: 'token'
+            routes: {
+                token: RouteToken;
+                network: NetworkRoute;
+            }[]
+        }
+    )
